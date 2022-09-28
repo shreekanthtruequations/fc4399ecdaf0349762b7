96fa35f60f716421e1ed755d435430d3445de4c9
shreekanthtruequations <shreekanth.bommidi@truequations.com>
2022-09-28 16:27:40 +0530

    api

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :robot do
+      post ':id/orders', to: 'orders#create'
+    end
+  end
 end
--- db/migrate/20220928100802_create_robots.rb
+++ db/migrate/20220928100802_create_robots.rb
@@ -1,6 +1,9 @@
 class CreateRobots < ActiveRecord::Migration[6.1]
   def change
     create_table :robots do |t|
+      t.string "facing"
+      t.integer "x_coordinate", default: 0
+      t.integer "y_coordinate", default: 0
 
       t.timestamps
     end
